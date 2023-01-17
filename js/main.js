3a8bcf8c8578ea122f3e147a51eacb3005066373
Aalaizha <l.bnjamin0309@gmail.com>
2023-01-17 01:44:25 -0500

    ADD : Mercury Fact Page & Styling

--- js/main.js
+++ js/main.js
@@ -1,41 +1,61 @@
+let wrapper = document.querySelector(".wrapper");
 
 let loadMercury = () => {
+  clearDom();
 
-}
+  let planetWrapper = document.createElement("div");
+  planetWrapper.classList.add("planetFactsWrapper");
+  wrapper.append(planetWrapper);
 
-let loadVenus = () =>{
+  let mercury = document.createElement("img");
+  mercury.src = "../img/Mercury.svg";
+  mercury.classList.add("mercuryImg");
+  planetWrapper.append(mercury);
 
-}
+  let planetName = document.createElement("h1");
+  planetName.innerText = "Mercury";
+  planetWrapper.append(planetName);
 
-let loadEarth = () =>{
-    
-}
+  let planetInfo = document.createElement("p");
+  planetInfo.innerText =
+    "Mercury is the smallest planet in our solar system and closest to the Sun. It is only slightly larger than Earth's Moon. \n \n Mercury is the fastest planet, zipping around the Sun every 88 Earth days.";
 
-let loadMars = () =>{
+  planetWrapper.append(planetInfo);
 
-}
+  let backBtn = document.createElement("a");
+  backBtn.innerText = "< Back";
+  backBtn.classList.add("backBtn");
+  planetWrapper.append(backBtn);
 
-let loadJupiter = () =>{
+  backBtn.addEventListener("click", () => {
+    location.reload();
+  });
+};
 
-}
+let loadVenus = () => {};
 
-let loadSaturn = () =>{
+let loadEarth = () => {};
 
-}
+let loadMars = () => {};
 
-let loadUranus = () =>{
-    
-}
+let loadJupiter = () => {};
 
-let loadNeptune = () => {
+let loadSaturn = () => {};
 
-}
+let loadUranus = () => {};
 
-document.querySelector("#loadMercury").addEventListener("click", loadMercury)
-document.querySelector("#loadVenus").addEventListener("click", loadVenus)
-document.querySelector("#loadEarth").addEventListener("click", loadEarth)
-document.querySelector("#loadMars").addEventListener("click", loadMars)
-document.querySelector("#loadJupiter").addEventListener("click", loadJupiter)
-document.querySelector("#loadSaturn").addEventListener("click", loadSaturn)
-document.querySelector("#loadUranus").addEventListener("click", loadUranus)
-document.querySelector("#loadNeptune").addEventListener("click", loadNeptune)
+let loadNeptune = () => {};
+
+//Clears the UI
+let clearDom = () => {
+  wrapper.innerHTML = "";
+};
+
+document.querySelector("#loadMercury").addEventListener("click", loadMercury);
+document.querySelector("#loadVenus").addEventListener("click", loadVenus);
+document.querySelector("#loadEarth").addEventListener("click", loadEarth);
+document.querySelector("#loadMars").addEventListener("click", loadMars);
+document.querySelector("#loadJupiter").addEventListener("click", loadJupiter);
+document.querySelector("#loadSaturn").addEventListener("click", loadSaturn);
+document.querySelector("#loadUranus").addEventListener("click", loadUranus);
+document.querySelector("#loadNeptune").addEventListener("click", loadNeptune);
