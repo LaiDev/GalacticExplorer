5c57ee7fc7525b18b10bd505e719a0447625dfd3
Aalaizha <l.bnjamin0309@gmail.com>
2023-01-16 20:02:38 -0500

    ADD : Reference to "Learn More Buttons"

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+let loadMercury = () => {
+
+}
+
+let loadVenus = () =>{
+
+}
+
+let loadEarth = () =>{
+    
+}
+
+let loadMars = () =>{
+
+}
+
+let loadJupiter = () =>{
+
+}
+
+let loadSaturn = () =>{
+
+}
+
+let loadUranus = () =>{
+    
+}
+
+let loadNeptune = () => {
+
+}
+
+document.querySelector("#loadMercury").addEventListener("click", loadMercury)
+document.querySelector("#loadVenus").addEventListener("click", loadVenus)
+document.querySelector("#loadEarth").addEventListener("click", loadEarth)
+document.querySelector("#loadMars").addEventListener("click", loadMars)
+document.querySelector("#loadJupiter").addEventListener("click", loadJupiter)
+document.querySelector("#loadSaturn").addEventListener("click", loadSaturn)
+document.querySelector("#loadUranus").addEventListener("click", loadUranus)
+document.querySelector("#loadNeptune").addEventListener("click", loadNeptune)
